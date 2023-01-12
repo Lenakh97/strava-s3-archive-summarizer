6dafc2f3dac4a07cad17c4483d7e39e73a01c715
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-01-12 14:17:48 +0100

    feat: add function for dividing archive into weekly array

---
+++ lib/divideIntoWeeks.ts
@@ -0,0 +1,23 @@
+import { readdir } from "fs/promises"
+import { getFirstDate } from "./divideArchiveInWeeks.js"
+
+export const divideIntoweeks = async (weekNumber: number, folderName: string) => {
+    
+	const firstFileName = await getFirstDate()
+	const firstDateWeek1 = new Date(firstFileName.slice(0, 10)).getTime()
+	const startDate = new Date(
+		firstDateWeek1 + 1000 * 60 * 60 * 24 * 7 * (weekNumber - 1),
+	)
+	const finishDay = new Date(
+		firstDateWeek1 + 1000 * 60 * 60 * 24 * 7 * weekNumber,
+	)
+	const weeklyData = []
+	const fileArray = await readdir(folderName)
+	for (const file of fileArray) {
+		const fileDate = new Date(file.slice(0, 10))
+		if (fileDate >= startDate && fileDate < finishDay) {
+			weeklyData.push(file)
+		}
+	}
+	return weeklyData
+}

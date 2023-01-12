dcc0ad622d92805a0ae6d4db7019a9e094bf90d2
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-01-12 14:20:28 +0100

    feat: add function for dividing archive into club and week

---
+++ lib/divideArchiveInClubAndWeek.ts
@@ -0,0 +1,19 @@
+import { divideIntoTeams, teamsSummary } from "./divideIntoTeams";
+import { divideIntoweeks } from "./divideIntoWeeks";
+
+type ArchiveDivided = Record<number, {
+	teams: teamsSummary;}>
+
+
+export const divideArchiveInClubAndWeek = async (): Promise<ArchiveDivided> =>{
+	const divideArchive = {} as ArchiveDivided
+	for (let week=1; week<5; week++){
+		const weekData = await divideIntoweeks(week, './archive')
+		console.log(weekData)
+		const teamDivision = divideIntoTeams(weekData) as teamsSummary
+		if(divideArchive[week] === undefined){
+			divideArchive[week] = {teams: teamDivision}
+		}
+	}
+	return divideArchive
+}

0c67d8e5d822f10a89997462bb38ef6edb2dc9ea
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-01-20 14:03:09 +0100

    fix: change from week 1-4 to getting only current week

--- lib/divideIntoWeeks.ts
+++ lib/divideIntoWeeks.ts
@@ -1,21 +1,13 @@
 import { readdir } from "fs/promises"
-import { getFirstDate } from "./getFirstDate.js"
+import { weekNumber } from "./weekNumber.js"
 
-export const divideIntoweeks = async (weekNumber: number, folderName: string) => {
-    
-	const firstFileName = await getFirstDate()
-	const firstDateWeek1 = new Date(firstFileName.slice(0, 10)).getTime()
-	const startDate = new Date(
-		firstDateWeek1 + 1000 * 60 * 60 * 24 * 7 * (weekNumber - 1),
-	)
-	const finishDay = new Date(
-		firstDateWeek1 + 1000 * 60 * 60 * 24 * 7 * weekNumber,
-	)
+export const divideIntoweeks = async (week: number, folderName: string) => {
 	const weeklyData = []
 	const fileArray = await readdir(folderName)
-	for (const file of fileArray) {
-		const fileDate = new Date(file.slice(0, 10))
-		if (fileDate >= startDate && fileDate < finishDay) {
+	for (const file of fileArray){
+		const fileDate = new Date (file.slice(0,10))
+		const currentWeek = weekNumber(fileDate)
+		if (currentWeek === week){
 			weeklyData.push(file)
 		}
 	}

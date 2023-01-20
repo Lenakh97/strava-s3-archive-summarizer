edd74080677c201f0d31cd880f5b4fd340e5bd0e
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-01-20 14:09:06 +0100

    fix: update winner AND file when duplicate winner and add comment

--- lib/pickRandomWinners.ts
+++ lib/pickRandomWinners.ts
@@ -3,6 +3,13 @@ import path from 'path'
 import { Winners } from '../sendWinners'
 import { ArchiveDivided } from './divideArchiveInClubAndWeek'
 
+/*
+This function will get random winners in each team for the picked weeknumber. 
+If the winner is already picked it will try 150 times to get a new winner by 
+picking new files and new random winners. If the winner is not unique after 150
+tries we will get the same winner multiple times. This is done in case <5 people
+are active in a club. If not we would have an infinite loop.
+ */
 export const pickRandomWinners = async (
 	weekNumber: number,
 	divided: ArchiveDivided,
@@ -11,11 +18,10 @@ export const pickRandomWinners = async (
 	const teams = divided[`${weekNumber}`].teams
 	for (const team in teams) {
 		let JSONData = await getJSONData(divided, weekNumber, team)
-		console.log(JSONData)
 		for (let person = 0; person < 4; person++) {
 			let randomPerson =
 				JSONData[Math.floor(Math.random() * JSONData.length)].athlete
-			let str = randomPerson.firstname + ', ' + randomPerson.lastname + '; '
+			let str = randomPerson.firstname + ', ' + randomPerson.lastname
 			if (winners[team] === undefined) {
 				winners[team] = {
 					winner: [str],
@@ -23,9 +29,10 @@ export const pickRandomWinners = async (
 			}
 			let counter = 0
 			while (winners[team].winner.includes(str) && counter < 150) {
+				JSONData = await getJSONData(divided, weekNumber, team)
 				let rnd = Math.random()
 				randomPerson = JSONData[Math.floor(rnd * JSONData.length)].athlete
-				str = randomPerson.firstname + ', ' + randomPerson.lastname + '; '
+				str = randomPerson.firstname + ', ' + randomPerson.lastname
 				counter += 1
 			}
 			winners[team].winner.push(str)
